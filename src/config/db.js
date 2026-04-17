7c4413be183cdcd80b3d3b9e35204b6bca23edc4
Het-Roy <het.roy.cg@gmail.com>
2026-04-17 22:25:38 +0530

    feat: add .env endpoint

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ConnectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/Notesapp");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to DB");
     } catch (error) {
         console.log(error.message);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const ConnectDB = require('./config/db');
 const app = require('./app');
 
 ConnectDB();
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
